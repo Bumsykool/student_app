d1010cb7e3f72bea88524091c61f11db12d0462c
Olubunmi <Olubunmi>
2017-01-06 17:30:15 +0100

    update

--- delete.php
+++ delete.php
@@ -7,58 +7,25 @@ require 'db/connect.php';
 <?php
 
 
-$id = $_GET['user_id'];
-$idcount= count ($id);
-
-
-if(isset($id))
-{
-	for($i=0;$i<$idcount;$i++){
-
-		$del = $id[$i];
-		$delete = "DELETE FROM students WHERE id={$id}" .$del;
-
-if (mysqli_query($db, $delete)) {
-
-    echo "Record deleted successfully";
-} else {
-    echo "Error deleting record: " . mysqli_error($db);
-}
-
-	}
 
 
 
-}
 
-
-if ($result = $db->query("SELECT * FROM students")){
-		if($count = $result->num_rows){
-
-		echo '<p>' ,$count  ,'</p>'; 
+  if ($result = $db->query("SELECT * FROM students")){
+		
 		$rows = $result->fetch_all(MYSQLI_ASSOC);
     
 
     foreach ($rows  as $row ) {
-    	echo $row [id];
+    	//echo $row [id];
+    	$id = $_GET['id'];
     }
-		echo '<pre>' , print_r($rows), '</pre>' ;
-
-	}
+		//echo '<pre>' , print_r($rows), '</pre>' ;
 }
+	
 
-		//<a href='list.php?id=$user_id'>
 
-		
-
-// sql to delete a record
-// if ($result = $db->query("SELECT * FROM students")) {
- //	if($results->num_rows) {
- //		echo 'yay';
- //	}
-// }
-
-/*  $delete = "DELETE FROM students WHERE id={$id}";
+  $delete = "DELETE FROM students WHERE id=$id";
 
 if (mysqli_query($db, $delete)) {
 
@@ -66,5 +33,5 @@ if (mysqli_query($db, $delete)) {
 } else {
     echo "Error deleting record: " . mysqli_error($db);
 }
-*/
+
 ?>
